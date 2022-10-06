fcb6984074236fa44e1e12017fd4672dfa56704e
Braii <llamoccas.brian@gmail.com>
2022-10-06 00:02:16 +0000

    part2: new associations

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -6,7 +6,7 @@ class Attack < ApplicationRecord
 
     def monster_with_scare_level_higer_than_five_cant_attack_children
         if ((monster.scare_level > 5) && ((Date.today.year - victim.birthdate.year) < 18))
-            errors.add (:base, :invalid, message: "Este ataque es invalido porque #{monster.name} es un monstruo con nivel de susto mayor al 5 (#{monster.scare_level}) y #{victim.name} es una victima menor de edad")
+            errors.add :base, :invalid, message: "Este ataque es invalido porque #{monster.name} es un monstruo con nivel de susto mayor al 5 (#{monster.scare_level}) y #{victim.name} es una victima menor de edad"
         end
     end
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,6 +1,6 @@
 class Monster < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     
+    has_many :attacks, dependent: :destroy
     has_many :victims, through: :attacks
-    has_many :attacks
 end
